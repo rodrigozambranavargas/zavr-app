a767f721727e6f1b706051a498b99a9cea54834b
rodrigo.zambrana <rodrigo.zambrana.vargas@gmail.com>
2020-03-10 20:03:14 -0300

    AWS configuration

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// React
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
@@ -5,22 +6,30 @@ import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 // styles
 import "assets/css/bootstrap.min.css";
 import "assets/scss/paper-kit.scss";
-//import "assets/demo/demo.css";
 
-// pages
+// Pages
 import ZavrPTIndex from "views/ZavrPTIndex.js";
-//import RegisterPage from "views/examples/RegisterPage.js";
+import Login from "views/Login.js";
 
-// others
+// AWS
+import Amplify from "aws-amplify"; // AWS Amplify es una plataforma de desarrollo para la creación de aplicaciones móviles y web seguras y escalables.
+import config from "AWS/config";
+
+// Configuration for AWS Cognito
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID // Base de datos
+  }
+});
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/indexPT" render={props => <ZavrPTIndex {...props} />} />
-      {/*  <Route
-        path="/register-page"
-        render={props => <RegisterPage {...props} />}
-      /> */}
+      <Route path="/Login" render={props => <Login {...props} />} />
       <Redirect to="/indexPT" />
     </Switch>
   </BrowserRouter>,
